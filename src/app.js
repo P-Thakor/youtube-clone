28c9a87831934761264ac880b291f05a23f06f22
P-Thakor <pthakor.work@gmail.com>
2024-09-27 20:48:31 +0530

    created user route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+console.log('Main');
 const app = express();
 
 app.use(
@@ -15,5 +16,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//router declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
